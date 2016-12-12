8aaec8efd0b3b3006325d9cdec36f5d7f53eb882
Jstrycker <jstryckman@gmail.com>
2016-12-12 21:55:43 +0000

    frist tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
     validates :name, presence: true
+    validates :name, length:{minimum: 2}
+    validates :name, length:{maximum: 30}
+    validates :name, uniqueness: true, on: :create
     validates :email, presence: true
+    validates :email, uniqueness: true, on: :create	
     validates :password, presence: true
+    validates :password, length:{minimum: 5}
 end
