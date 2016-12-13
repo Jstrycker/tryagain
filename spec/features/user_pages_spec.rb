7c9acf42367773689c5ddbed8ecfa63821966de8
Jstrycker <jstryckman@gmail.com>
2016-12-13 04:44:23 +0000

    akdjfhosdhfoa

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -5,7 +5,7 @@ describe "User Pages" do
 		describe 'all' do
 			before do
 		25.times {|i| FactoryGirl.create(:user)}
-				visit '/users'
+				visit users_path
 			end
 	it{should have_content('List of users')}
 	it{should have_content('25 users')}
@@ -22,48 +22,53 @@ describe "User Pages" do
 
 end
 
+
 describe "creating User" do
-	before {visit "/users/new"}
+   	before {visit new_user_path}
 		it "Hides password test" do 
 			should have_field('user_password' ,type: 'password')
-end
-describe "with invalid information" do
-	it "does not add new user" do
+		end
 
-		expect{click_button 'Submit' }.not_to change(User, :count)
+	describe "with invalid information" do
+		it "does not add new user" do
 
+			expect{click_button 'Submit' }.not_to change(User, :count)
+
+		end
+
+		it 'produces error message'do
+
+			should have_alert(:danger)
+		end
 	end
 
-	it 'produces error message'do
 
+	describe "With valid information" do
+		before do
+			fill_in 'Username', with: "Username"
+			fill_in 'Email', with: "Something@taylor.edu"
+			fill_in 'password', with: "SecurePassword"
+		end
 
-	should have_alert(:danger)
-	end
-end
-
-
-describe "With valid information" do
-	before do
-		fill_in 'Username', with: "Username"
-		fill_in 'Email', with: "Something@taylor.edu"
-		fill_in 'Password', with: "SecurePassword"
-	end
-
-it "allows the user to fill the feilds" do
-	click_button 'Submit'
-	end
-	it "does add new user" do
-
-		expect{click_button 'Submit' }.to change(User, :count).by(1)
-
-	end
-	it 'produces welcome message'do
-	before {click_button 'Submit'} 
-
-	should have_alert(:success,text: 'Welcome')
+		it "allows the user to fill the feilds" do
+			click_button 'Submit'
+		end
 	
+		it "does add new user" do
+
+			expect{click_button 'Submit' }.to change(User, :count).by(1)
+
+		end
+	
+		describe 'produces welcome message' do
+	
+			before do 
+				click_button 'Submit'
+				end 
+
+			should have_alert(:success, text: 'Welcome')
+		end
+
+		end
+
 	end
-
-end
-
-end
